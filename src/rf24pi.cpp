ac41a808adc0f1742d39c4411c2bc1af9c26bd6d
Nicolas Giai <nicogiai@gmail.com>
2021-05-23 12:52:45 -0300

    mejora script daemon. imprime en syslog

--- src/rf24pi.cpp
+++ src/rf24pi.cpp
@@ -1,8 +1,12 @@
-#include <stdio.h>
+#include <iostream>    // cin, cout, endl
+#include <syslog.h>
 
 int main(void) {
 
-	printf("Hello from autotools!\n");
+
+	std::cout << "Hello from autotools!" << std::endl;
+	syslog (LOG_INFO, "Hello from autotools!");
+
 	return 0;
 
 }
